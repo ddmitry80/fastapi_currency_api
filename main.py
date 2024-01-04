2662b5ba6294ec872d0d66f74f86466956fbe3ab
Dmitry Dementiev <ddmitry@gmail.com>
2024-01-04 21:57:17 +0300

    change imports in main

--- main.py
+++ main.py
@@ -2,13 +2,12 @@ import logging
 from logging import config as logging_config
 logging_config.fileConfig('logging.ini')
 
-from app.api.dependencies.db import UOWDep
-from app.db.database import init_db_schema
-from app.services.user import UserService
-
 import uvicorn
 from fastapi import FastAPI
 
+from app.api.dependencies.db import UOWDep
+from app.db.database import init_db_schema
+from app.services.user import UserService
 from app.api.routers.routers import all_routers
 
 logger = logging.getLogger(__name__)
