fca025c6b67fa4d46c5230159c85409aad8ca50c
Dmitry Dementiev <ddmitry@gmail.com>
2024-01-06 20:30:55 +0300

    Второй тест - не работает

--- tests/test_integration_db.py
+++ tests/test_integration_db.py
@@ -1,7 +1,10 @@
 import logging
 from logging import config as logging_config
+from httpx import AsyncClient
 
 from sqlalchemy import NullPool
+
+from app.api.schemas.auth import UserCreate
 logging_config.fileConfig('logging.ini')
 
 import pytest
@@ -18,8 +21,6 @@ logger = logging.getLogger(__name__)
 
 @pytest_asyncio.fixture(scope="session")
 async def async_db_sesstion():
-    # temp_database_uri = "sqlite:///:memory:"
-    # temp_database_uri = "sqlite+aiosqlite:///test_sqlite.db"
     temp_database_uri = "postgresql+asyncpg://postgres:postgres@localhost:5433/test"
     engine = create_async_engine(temp_database_uri, echo=True, poolclass=NullPool)
     async_session_maker = async_sessionmaker(engine, class_=AsyncSession)
@@ -35,24 +36,29 @@ async def async_db_sesstion():
 
 
 # @pytest.mark.anyio
-@pytest_asyncio.fixture(scope="session")
+@pytest_asyncio.fixture()
 async def mock_uow(async_db_sesstion) -> UnitOfWork:
 
     uow = UnitOfWork(session_factory=async_db_sesstion)
 
     print(f"UnitOfWork: init done")
 
-    # await app.db.database.init_db_schema()
     # Вставляю в БД тестовых пользователей
-    await UserService.insert_test_data(uow=uow)
-    return uow
+    async with uow:
+        await UserService.insert_test_data(uow=uow)
+        yield uow
 
 
 @pytest.mark.asyncio
 class TestDBIntegration:
     async def test_read_user(self, mock_uow, client):
-        print("mock_uow type:", type(mock_uow))
-        async with mock_uow:
-            user = await mock_uow.user.fetch_one(id=1)
+        # print("mock_uow type:", type(mock_uow))
+        # async with mock_uow:
+        user = await mock_uow.user.fetch_one(id=1)
         assert user.email == "u1@example.com"
         assert user.is_admin == True
+
+    async def test_create_user(self, mock_uow: UnitOfWork, client: AsyncClient):
+        new_user = UserCreate(email="test@example.com", password="A12345a!")
+        response = await client.post('/auth/users', json=new_user.to_log())
+        assert response.json()["email"] == new_user.email
