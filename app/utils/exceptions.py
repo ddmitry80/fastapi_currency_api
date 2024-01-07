e123b95af7862ab37d4922bb53e0fca4036992cb
Dmitry Dementiev <ddmitry@gmail.com>
2024-01-07 23:13:56 +0300

    Больше Exceptions

--- app/utils/exceptions.py
+++ app/utils/exceptions.py
@@ -32,3 +32,18 @@ class NotAuthenticated(DetailedHTTPException):
 
     def __init__(self) -> None:
         super().__init__(headers={"WWW-Authenticate": "Bearer"})
+
+
+class ExternalApiException(HTTPException):
+    def __init__(self, detail: str = "Something went wrong on external service", status_code: int = 500):
+        super().__init__(status_code=status_code, detail=detail)
+
+
+class BadCurrencyCode(HTTPException):
+    def __init__(self, detail: str = "Bad user request (maybe bad currency codes)", status_code: int = 400):
+        super().__init__(status_code=status_code, detail=detail)
+
+
+class CurrencyZeroRate(HTTPException):
+    def __init__(self, detail: str = "One of currency rates is zero (division by zero)", status_code: int = 500):
+        super().__init__(status_code=status_code, detail=detail)
