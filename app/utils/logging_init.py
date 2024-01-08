33d2385a3f16f7f9d304c47951dbff0d0eb7b4b7
Dmitry Dementiev <ddmitry@gmail.com>
2024-01-08 17:15:15 +0300

    Инициализация логирования через yml

--- app/utils/logging_init.py
+++ app/utils/logging_init.py
@@ -1,5 +1,34 @@
 import logging
 from logging import config as logging_config
+import os
+import yaml
 
-# Загрузка конфига логгера
-logging_config.fileConfig('logging.ini')
+
+def setup_logging(default_path='logging.yaml', default_level=logging.INFO, env_key='LOG_CFG'):
+    """
+    | **@author:** Prathyush SP
+    | Logging Setup
+    """
+    path = default_path
+    env_path = os.getenv(env_key, None)
+    if env_path:
+        path = env_path
+    if os.path.exists(path):
+        with open(path, 'rt', encoding='utf8') as f:
+            try:
+                # config = yaml.safe_load(f.read())
+                config = yaml.unsafe_load(f.read())
+                logging_config.dictConfig(config)
+                # coloredlogs.install()
+            except Exception as e:
+                print(e)
+                print('Error in Logging Configuration. Using default configs')
+                logging.basicConfig(level=default_level)
+                # coloredlogs.install(level=default_level)
+    else:
+        logging.basicConfig(level=default_level)
+        # coloredlogs.install(level=default_level)
+        print('Failed to load configuration file. Using default configs')
+
+setup_logging(default_level=logging.DEBUG)
+
